690d4a5ffa6b0ce1cdedfc174e802cc62c14bb68
Fernando <fmcampanaro@gmail.com>
2022-07-07 11:46:22 -0300

    Banco de dados
    Criação do banco de dados.
     07/07/2022

---
+++ DB/sitesigma.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 07-Jul-2022 às 16:40
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `sitesigma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `CategoriaID` int(11) NOT NULL,
+  `Nome` varchar(100) NOT NULL,
+  `Ativo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `categorias`
+--
+
+INSERT INTO `categorias` (`CategoriaID`, `Nome`, `Ativo`) VALUES
+(1, 'Capivaras', 1),
+(2, 'Sigmas', 1),
+(3, 'Calvos', 1),
+(9, 'Chads', 1),
+(10, 'Classic', 1),
+(11, 'Limitados\r\n', 1),
+(12, 'Pedra', 1),
+(13, 'Cachorros', 1),
+(14, 'Gatos', 1),
+(15, 'Melancias', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `ProdutoID` int(11) DEFAULT NULL,
+  `Nome` varchar(150) NOT NULL,
+  `Descricao` varchar(250) NOT NULL,
+  `Imagem` varchar(200) NOT NULL,
+  `Preco` float NOT NULL,
+  `Categoria` int(11) NOT NULL,
+  `Ativo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`CategoriaID`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `CategoriaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
