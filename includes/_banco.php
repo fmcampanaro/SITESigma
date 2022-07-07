fbb2c59b0deb4a40cac652a55a9183af33248c17
Fernando <fmcampanaro@gmail.com>
2022-07-07 12:25:07 -0300

    Banco de dados incluido
    DB incluida
    07/07/2022

--- includes/_banco.php
+++ includes/_banco.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $database = "sitesigma";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
