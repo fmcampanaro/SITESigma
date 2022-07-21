9ab4060747563216563395a4b9c93e14833546b9
Fernando <fmcampanaro@gmail.com>
2022-07-21 12:19:35 -0300

    Banco de dados
    Cards do index funcionando com o banco de dados

--- teste.php
+++ teste.php
@@ -5,7 +5,8 @@ foreach ($produtos as $key => $value){
     $nome = $value["nome"];
     $desc = $value["descricao"];
     $img = $value["imagem"];
-    $rs = $value["preco"];
-    echo "INSERT INTO `produtos` (`ProdutoID`, `Nome`, `Descricao`, `Imagem`, `Preco`, `Categoria`, `Ativo`) VALUES ('', '$nome', '$desc', '$img', '$rs', '1', '1');<br>";
+    $rs = str_replace("," , ".", $value["preco"]);
+    echo "INSERT INTO `produtos` (`ProdutoID`, `Nome`, `Descricao`, `Imagem`, `Preco`, `Categoria`, `Ativo`) VALUES (NULL, '$nome', '$desc', '$img', '$rs', '1', '1');<br>";
 };
+
 ?>
