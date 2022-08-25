43e51d608b55710086f18810b46e40cbf99dba6b
Fernando <fmcampanaro@gmail.com>
2022-08-25 12:07:13 -0300

    correção

---
+++ admin/categoria-lista.php
@@ -0,0 +1,63 @@
+<?php
+//inclui o banco
+include_once '../includes/_banco.php';
+//inclui o head
+include_once '../includes/_head.php';
+
+$sql = "SELECT * FROM categorias";
+$resultado = mysqli_query($conn,$sql);
+$total = mysqli_num_rows($resultado);
+
+include_once '_menu.php';
+?>
+
+<main>
+    <h2>Admistração das categorias</h2>
+    <a href="categoria-salvar.php">Inserir</a>
+    <hr>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Ações</th>
+        </tr>
+        <?php
+        if ($resultado){
+            while ($dado = mysqli_fetch_array($resultado)){
+        ?>
+
+        <tr>
+            <td><?php echo $dado['CategoriaID'];?></td>
+            <td><a href="categoria-salvar.php?acao=salvar&id=<?php echo $dado['CategoriaID'];?>"><?php echo $dado['Nome'];?></a></td>
+            <td><a href="categoria-processa.php?acao=excluir&id=<?php echo $dado['CategoriaID'];?>">Excluir</a></td>
+
+        </tr>
+
+        <?php
+        }
+        }else {
+
+        ?>
+        
+        <tr>
+            <td colspan='3'>Resultados não encontrados</td>
+        </tr>
+
+        <?php
+        }
+        
+        ?>
+
+        <tr>
+            <td colspan='3'>Total de Registros: <?php echo $total;?></td>
+        </tr>
+
+    </table>
+</main>
+
+
+<?php
+//inclui o footer do painel admin
+include_once './_footer.php';
+
+?>
---
+++ admin/categoria-salvar.php
@@ -0,0 +1,58 @@
+<?php
+//inclui o banco
+include_once '../includes/_banco.php';
+//inclui o head
+include_once '../includes/_head.php';
+
+if (isset($_GET['id']) || !empty($_GET['id'])){
+    $id = $_GET['id'];
+
+    $sql = "SELECT * FROM categorias";
+    $resultado = mysqli_query($conn,$sql);
+    $dados = mysqli_fetch_array($resultado,MYSQLI_ASSOC);
+    }else{
+      $id = '';
+      $dados['Nome'] = '';
+      $dados['Descrição'] = '';
+      $dados['Imagem'] = '';  
+
+    
+    }
+
+include_once '_menu.php';
+?>
+
+<main>
+    <h2>Admistração de Categorias</h2>
+    <a href="categoria-lista.php">Listagem</a>
+    <hr>
+    <form action="categoria-processa.php" method="post" enctype="multipart/form-data">
+        <input type="hidden" value="salvar" name="acao">
+        <input type="text" value="<?php echo $dados['Imagem'];?>" name="imagem">
+        <input type="text" name="id" value="<?php echo $id;?>"><br>
+        <label for="nome">Nome:</label><br>
+        <input type="text" id="nome" name="nome" value="<?php echo $dados['Nome'];?>"><br>
+        <label for="descricao">Descrição:</label><br>
+        <textarea name="descricao" id="descricao"><?php echo $dados['Descricao'];?></textarea><br>
+        <label for="imagem">Imagem:</label><br>
+        <?php
+        if (!empty($dados['Imagem']) ) {
+        ?>
+        <img src="../imagem/categorias/<?php echo $dados['Imagem'];?>" width="150" /><br>
+        <?php
+            
+        }
+        ?>
+    <input type="file" name="foto">
+     <hr>   
+    <input type="submit" value="Enviar">
+
+    </form>
+</main>   
+
+
+<?php
+//inclui o footer do painel admin
+include_once './_footer.php';
+
+?>
